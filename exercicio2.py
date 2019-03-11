0e86cdf75efe861b0e2d104ca415a32146922b50
thalissaj <thalissa.correa@loggi.com>
2019-03-11 19:31:11 -0300

    up the exercises

--- exercicio2.py
+++
@@ -1,8 +0,0 @@
-''
-    else:
-        tentativas -= 1
-        print("Errado! %s, ainda lhe restam %d tentativas" %(name_player, tentativas))
-        if palpite > numero_secreto:
-            print("Sua tentativa era maior que o numero secreto")
-        else:
-            print("Seu palpite era menor que o numero secreto")
